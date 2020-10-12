9945bde87ea5bc74f90d2b96c117e2412c1077da
Mikita Aleksakhin <MAleksakhin@gomel.iba.by>
2020-10-12 09:31:36 +0300

    changes for task 02

--- ansible.02.ad-hoc/adhoc01.sh
+++ ansible.02.ad-hoc/adhoc01.sh
@@ -4,8 +4,8 @@ ansible all -m ping -i all
 
 ansible all -m user -a "name=ansible createhome=yes"
 
-ansible all -m shell -a "mkdir /home/ansible/.ssh; touch /home/ansible/.ssh/authorized_keys"
-ansible all -m copy -a "src=/home/ansible/.ssh/id_rsa.pub dest=/home/ansible/.ssh/id_rsa.pub"
-ansible all -m shell -a "cat /home/ansible/.ssh/id_rsa.pub >> /home/ansible/.ssh/authorized_keys"
+ansible all -m shell -a "rm -rf /home/ansible/.ssh"
+
+ansible all -m authorized_key -a "user=ansible state=present key='{{ lookup('file', '/home/ansible/.ssh/id_rsa.pub') }}'" 
 
 ansible all -m lineinfile -a "dest=/etc/sudoers state=present line='%ansible ALL=(ALL:ALL) NOPASSWD: ALL'"
